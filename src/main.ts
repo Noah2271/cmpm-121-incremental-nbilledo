c4e1d7b74410469eca863e66b5027d60fe463b8a
nbilledo <nbilledo@ucsc.edu>
2025-10-06 23:42:22 +0000

    Added counter for times the bread button was pressed (Step 2 Complete)

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,25 @@
 //<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 
 import "./style.css";
+// variables
+let counter: number = 0;
+
+// html
 document.body.innerHTML = `
-<div class = "button-container">
-  <button class="button" type = "button">🍞</button>
-</div>
-  `;
+  <div class="button-container">
+    <button class="button" type="button" id="increment">🍞</button>
+  </div>
+  <div class="text-container">
+    <p id="counter" class="centered-text">0</br>Loaves of Bread</p>
+  </div>
+`;
+
+//Event Listeners
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = `${counter} <br> Loaves of Bread`;
+  console.log("woop");
+});
